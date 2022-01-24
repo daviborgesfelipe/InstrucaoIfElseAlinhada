2792cef527d8bc38751d023426a5c4df72dfc789
Davi Borges <davifelipe_300@hotmail.com>
2022-01-24 18:47:57 -0300

    Instrucao If e Else alinhada

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace InstrucaoIfElseAlinhada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float media = 2.6f;
+            bool aprovado = media >= 6;
+            bool recuperacao = media >=5;
+
+            if(aprovado)
+            {
+                System.Console.WriteLine("Aluno Aprovado!");
+            }
+            else if(recuperacao)
+            {
+                System.Console.WriteLine("Aluno em Recuperacao!");
+            }
+            else
+            {
+                System.Console.WriteLine("Aluno Reprovado!");
+            }
+        }
+    }
+}
